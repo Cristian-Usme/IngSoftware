62aa5e6190548f36c45467e68283058d6dedb279
Cristian Usme <Cristian1213u@gmail.com>
2024-07-25 13:37:05 -0500

    Conectar interface

--- app/src/main/java/com/upb/restaurant/MainActivity.java
+++ app/src/main/java/com/upb/restaurant/MainActivity.java
@@ -3,6 +3,8 @@ package com.upb.restaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void abrirMenu(View v){
+        Intent i;
+        i =  new Intent(this, MenuActivity.class);
+        startActivity(i);
+    }
+    public void abrirSede(View v){
+        Intent i;
+        i =  new Intent(this, SedeActivity.class);
+        startActivity(i);
+    }
+    public void abrirPedido(View v){
+        Intent i;
+        i =  new Intent(this, PedidoActivity.class);
+        startActivity(i);
+    }
+    public void abrirReserva(View v){
+        Intent i;
+        i =  new Intent(this, ReservaActivity.class);
+        startActivity(i);
     }
 }
